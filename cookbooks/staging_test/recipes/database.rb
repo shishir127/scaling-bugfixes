402c24d13d3ea0a188bdbe3f63a96cd310221fcc
shishir127 <shishir127@gmail.com>
2015-07-09 17:40:10 +0530

    Removed the database user setup becuase using AWS RDS

--- cookbooks/staging_test/recipes/database.rb
+++ cookbooks/staging_test/recipes/database.rb
@@ -1,33 +1,6 @@
-#include_recipe "postgresql::client"
-#include_recipe "postgresql::server"
-#include_recipe "database::postgresql"
+include_recipe "postgresql::client"
+include_recipe "postgresql::server"
 
 package["libpq-dev", "postgresql", "postgresql-contrib"]
 
-#gem_package "pg"
-
-#postgresql_connection_info = {
-#    host:     node[:sahai][:database][:host],
-#    port:     node[:sahai][:database][:port],
-#    username: node[:postgresql][:username][:postgres],
-#    password: node[:postgresql][:password][:postgres]
-#}
-
-#postgresql_database_user node[:sahai][:database][:username] do
-#   connection postgresql_connection_info
-#   password   node[:sahai][:database][:password]
-#   action     :create
-# end
-
-# postgresql_database node[:sahai][:database][:dbname] do
-#   connection postgresql_connection_info
-#   provider   Chef::Provider::Database::Postgresql
-#   action    :create
-# end
-
-# postgresql_database_user node[:sahai][:database][:username] do
-#   connection postgresql_connection_info
-#   database_name node[:sahai][:database][:dbname]
-#   privileges [:all]
-#   action :grant
-# end
+gem_package "pg"
